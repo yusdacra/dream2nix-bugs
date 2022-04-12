3952ec5f1779157a3fb7740b4cad79ba0769be3d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-04-12 06:32:47 +0300

    chore: update flake lock

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.dream2nix.url = "path:/home/patriot/Belgeler/projects/dream2nix";
+  inputs.dream2nix.url = "github:yusdacra/dream2nix/feat/cargo-lock-subpackages";
   outputs = { self, dream2nix }@inputs:
     let
       dream2nix = inputs.dream2nix.lib.init {
